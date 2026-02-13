e3afa3a93855bfff68b2e5fb104b377c00ad72c2
Faysal <82079632+FJay81@users.noreply.github.com>
2026-02-13 20:07:49 +0000

    Delete LearnJava directory

--- LearnJava/L2.java
+++
@@ -1,9 +0,0 @@
-//lesson 2
-public class L2 {
-    public static void main(String[] args){ //methods
-        System.out.println("Hello people");
-    }
-
-    String x = new String("sup"); //property //creates variable string "x"
-}
-
